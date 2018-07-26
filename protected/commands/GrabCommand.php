beb8cbddfe542ce579d68b8d05246ed98c4010bc
sunlyliuh <sunlyliuh@163.com>
2018-07-26 16:46:45 +0800

    edit

--- protected/commands/GrabCommand.php
+++ protected/commands/GrabCommand.php
@@ -1,21 +1,21 @@
 <?php
 /**
  * 抓取数据计划任务
- * @调用方式 /home/wwwroot/default/talk/datastatistic/protected/yiic grab action
+ * @调用方式 /home/wwwroot/default/talk/grablagou/trunk/protected/yiic grab action
  */
 class GrabCommand extends CConsoleCommand {
 
     /**
      * 抓取拉钩数据
-     * @执行方式 /home/wwwroot/default/talk/datastatistic/protected/yiic grab lagou
+     * @执行方式 /home/wwwroot/default/talk/grablagou/trunk/protected/yiic grab lagou
      */
     public function actionLagou()
     {
         
-        $jobArr  = array('php','Java','C','C++','Android','iOS');
+        $jobArr  = array('php','Java','C','C++','Android','iOS','python');
         $cityArr = array('北京','上海','广州','深圳','杭州','成都','武汉');
         $jobmodel = new Job;
-        $jobType = $jobArr[3];
+        $jobType = $jobArr[6];
         for($i=0; $i<count($cityArr); $i++){
             $city    = $cityArr[$i];
             for($page=1; $page<=10000;$page++){
@@ -24,16 +24,16 @@ class GrabCommand extends CConsoleCommand {
                 if($res){
                     $jobArr = json_decode($res,true);
                     if($jobArr['success'] == 1){
-                        $pageCount = $jobArr['content']['totalPageCount'];
+                        $pageCount = $jobArr['content']['positionResult']['totalCount'];
                         if($page > $pageCount){
                             echo 'grab over!';break;
                         }else{
                             echo "grab page:{$page}\n";
                         }
         //                print_r($jobArr['content']['result']);exit;
-                        foreach($jobArr['content']['result'] as $result){
+                        foreach($jobArr['content']['positionResult']['result'] as $result){
                             unset($result['companyLabelList']);
-                            $this->_writeLog('grab2.log', implode(',',$result));
+//                            $this->_writeLog('grab2.log', implode(',',$result));
                             $_job = clone $jobmodel;
                             //查找对应的id是否存在
                             $exist = $_job->exists("position_id=:position_id",array(':position_id'=>$result['positionId']));
@@ -44,11 +44,12 @@ class GrabCommand extends CConsoleCommand {
                                 $_job->salary      = $result['salary'];
                                 $_job->salary_min  = 0;
                                 $_job->salary_max  = 0;
-                                $_job->company_name= $result['companyName'];
+                                $_job->company_name= $result['companyFullName'];
                                 $_job->job_nature  = $result['jobNature'];
                                 $_job->education   = $result['education'];
                                 $_job->finance_stage = $result['financeStage'];
                                 $_job->company_size = $result['companySize'];
+                                $_job->lagou_create = $result['createTime'];
                                 $_job->createTime  = date('Y-m-d H:i:s');
                                 $_job->position_name= $result['positionName'];
                                 $res = $_job->save();
@@ -75,7 +76,7 @@ class GrabCommand extends CConsoleCommand {
     
     /**
      * etl清洗数据
-     * /home/wwwroot/default/talk/datastatistic/protected/yiic grab etl
+     * /home/wwwroot/default/talk/grablagou/trunk/protected/yiic grab etl
      */
     public function actionEtl()
     {
